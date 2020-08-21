55aab556a0c40ae2222954cd575f913b7a202c6d
Micheal Hansen <micheal.hansen@gmail.com>
2020-08-21 12:10:25 -0700

    add swagger document

--- ParkService/Startup.cs
+++ ParkService/Startup.cs
@@ -15,39 +15,61 @@ using ParkService.Models;
 
 namespace ParkService
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ParkContext>(opt =>
-                opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
-                
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            // app.UseHttpsRedirection();
-            app.UseMvc();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<ParkContext>(opt =>
+        opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+        
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+      services.AddSwaggerDocument(config =>
+      {
+        config.PostProcess = document =>
+        {
+          document.Info.Version = "v1";
+          document.Info.Title = "Park Service API";
+          document.Info.Description = "An api for storing and retrieving data about parks";
+          document.Info.Contact = new NSwag.OpenApiContact
+          {
+            Name = "Micheal Hansen",
+            Url = "https://github.com/sudolphus"
+          };
+          document.Info.License = new NSwag.OpenApiLicense
+          {
+            Name = "Use under MIT",
+            Url = "https://opensource.org/licenses/MIT"
+          };
+        };
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseOpenApi();
+      app.UseSwaggerUi3();
+      // app.UseHttpsRedirection();
+      app.UseMvc();
+    }
+  }
 }
