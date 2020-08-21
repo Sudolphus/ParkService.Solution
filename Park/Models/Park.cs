e4b025314ff948949f4ec54ce32b7da42c9641ec
Micheal Hansen <micheal.hansen@gmail.com>
2020-08-21 09:36:07 -0700

    add Park model

--- Park/Models/Park.cs
+++ Park/Models/Park.cs
@@ -1,4 +1,15 @@
-namespace ParkSerivce.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkService.Models
 {
-  
+  public class Park
+  {
+    public int ParkId { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+    [Required]
+    [StringLength(20)]
+    public string State { get; set; }
+  }
 }
